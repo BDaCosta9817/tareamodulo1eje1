0d586cda601e49016167b9ffeef13b79fdd2b30f
UserCP <UserCP@DESKTOP-EVBJTP8>
2024-11-03 20:42:26 -0600

    Carga inicial modulo1 ejercicio1

---
+++ src/main/java/com/mycompany/ejercicio1/Ejercicio1.java
@@ -0,0 +1,14 @@
+package com.mycompany.ejercicio1;
+
+/**
+ *Blanca Da Costa Gomez Carcamo
+ * 202310010031
+ * 1Programa para dar salida de mensaje 
+ */
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola, Soy Blanca Da Costa Gomez Carcamo");
+    }
+}
+
